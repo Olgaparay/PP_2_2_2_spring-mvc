cc09b8f924428d928ed3b3ee1b3173f61f36f3a1
Olgaparay <olgaparaeva@hotmail.com>
2024-06-11 23:44:40 +0300

    обработка HTTP-запросов и возвращение HTTP-ответов

--- src/main/java/web/controller/UserController.java
+++ src/main/java/web/controller/UserController.java
@@ -1,4 +1,57 @@
 package web.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+import web.model.User;
+import web.service.UserServiceImp;
+
+@Controller
+@RequestMapping("/users")
 public class UserController {
+    private UserServiceImp userService;
+
+    public UserController(UserServiceImp userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public String index(ModelMap model) {
+        model.addAttribute("users", userService.getAllUsers());
+        return "users";
+    }
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") Long id, Model model){
+        model.addAttribute("user", userService.getUserById(id));
+        return "show";
+    }
+    @GetMapping("/new")
+    public String newUser(@ModelAttribute("user")User user){
+        return "new";
+    }
+    @PostMapping()
+    public String create(@ModelAttribute("user") User user) {
+        userService.saveUser(user);
+        return "redirect:/users";
+    }
+    @GetMapping("/{id}/edit")
+    public String edit(@PathVariable("id") Long id, Model model) {
+        User user = userService.getUserById(id);
+        model.addAttribute("user", user);
+        return "edit";
+    }
+
+    @PatchMapping("/{id}")
+    public String update(@ModelAttribute("user") User user, @PathVariable("id") Long id) {
+        userService.updateUser(id, user);
+        return "redirect:/users";
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") Long id) {
+        userService.deleteUser(id);
+        return "redirect:/users";
+    }
+
 }
