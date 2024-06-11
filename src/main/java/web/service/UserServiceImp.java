eae7d5917462ba504c9c3fc9ac48635b1683ddd2
Olgaparay <olgaparaeva@hotmail.com>
2024-06-11 23:43:20 +0300

    создание интерфейса и реализация логики

--- src/main/java/web/service/UserServiceImp.java
+++ src/main/java/web/service/UserServiceImp.java
@@ -1,4 +1,42 @@
 package web.service;
 
-public class UserServiceImp {
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import web.dao.UserDao;
+import web.model.User;
+
+import java.util.List;
+@Service
+@Transactional
+public class UserServiceImp implements UserService{
+    private final UserDao userDao;
+
+    public UserServiceImp(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userDao.getAllUsers();
+    }
+
+    @Override
+    public User getUserById(Long id) {
+        return userDao.getUserById(id);
+    }
+
+    @Override
+    public void saveUser(User user) {
+        userDao.saveUser(user);
+    }
+
+    @Override
+    public void deleteUser(Long id) {
+        userDao.deleteUser(id);
+    }
+
+    @Override
+    public void updateUser(Long id, User user) {
+        userDao.updateUser(id, user);
+    }
 }
